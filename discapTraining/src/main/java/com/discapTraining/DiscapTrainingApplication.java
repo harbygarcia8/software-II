12bae2fac46e22437ce696395493c210c6673140
Harby Garcia <harby-96@hotmail.com>
2022-09-22 19:53:02 -0500

    Iniciando proyecto de software II

---
+++ discapTraining/src/main/java/com/discapTraining/DiscapTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.discapTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscapTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscapTrainingApplication.class, args);
+	}
+
+}
